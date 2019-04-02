e94c7da099483bd11d09fa70ebed417e60bf3585
Sam Mader <chesterpig96@gmail.com>
2019-04-02 19:39:47 -0400

    Still fixing

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,20 +1,4 @@
 package sample;
 
-import javafx.fxml.FXML;
-import javafx.scene.text.Text;
-
 public class Controller {
-	ourApplication a;
-	@FXML Text title;
-	@FXML Text description;
-	@FXML Text rating;
-	public void update() {
-		a = new ourApplication("Test", "This is a test.", 5, 0, 69, 0, null, null);
-		title = new Text();
-		title.setText(a.name);
-		description = new Text();
-		description.setText(a.description);
-		rating = new Text();
-		rating.setText(Double.toString(a.rating));
-	}
 }
